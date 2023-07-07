82637d1fa82b1b057750dc34e5357a786184e5eb
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-07-07 22:25:01 +0700

    Update sitemap.js

--- src/pages/api/sitemap.js
+++ src/pages/api/sitemap.js
@@ -10,7 +10,8 @@ export default function handler(req, res) {
       const xml = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
       <url>
-        <loc>http://www.example.com/foo.html</loc>
+        <loc>https://avseel.xyz/</loc>
+        <priority>1.0</priority>
         <lastmod>2021-01-01</lastmod>
       </url>
       </urlset>`
