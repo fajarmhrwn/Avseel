d8aecd10f03e4411ffe271643c02b9b2acb01f20
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-07-08 13:16:28 +0700

    Update Bottom.tsx

--- src/components/Section-5/Bottom.tsx
+++ src/components/Section-5/Bottom.tsx
@@ -5,11 +5,11 @@ const Bottom = () => {
     <div className='flex flex-col py-8 px-4 bg-[#334A37] items-center justicy-center'>
         <div className='sm:flex justify-center'>
             <img src="/images/sertifikat1_1.jpg" alt="" className='w-80 h-auto rounded-md sm:mr-12'/>
-            <img src="/images/sertifikat2_1.jpg" alt="" className='w-80 h-auto rounded-md mt-4'/>
+            {/* <img src="/images/sertifikat2_1.jpg" alt="" className='w-80 h-auto rounded-md mt-4'/> */}
         </div>
         <div className='sm:flex justify-center mt-12'>
             <img src="/images/sertifikat1_2.jpg" alt="" className='w-80 h-auto rounded-md  sm:mr-12'/>
-            <img src="/images/sertifikat2_2.jpg" alt="" className='w-80 h-auto rounded-md mt-4'/>
+            {/* <img src="/images/sertifikat2_2.jpg" alt="" className='w-80 h-auto rounded-md mt-4'/> */}
         </div>
         <div className='flex flex-col justify-center items-center mt-12'>
             <img src="/images/gubernur.jpg" alt="" className='w-80 h-auto rounded-md  sm:mr-12'/>
